b5724294ccabcf5dccf75159800098da6e2f1c25
Denys Sliusarenko <Denys.Sliusarenko@bd.com>
2021-07-29 12:46:22 +0300

    commit master 3

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!dsddsd");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
